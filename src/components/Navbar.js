892a0de0dffcb12cfafac12c989f2a92629657d1
Awaab Singh <adeoyin2828@gmail.com>
2022-07-27 10:49:38 +0100

    fe

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav class="navbar">
+        <div class="navbar-container container">
+           
+            <ul class="menu-items">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+            <h1 class="logo">Navbar</h1>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
